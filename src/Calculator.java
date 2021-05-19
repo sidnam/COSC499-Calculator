35caf4595f76120d1d9d50ba5bd5515ad0556683
sidnam <nambiar.siddesh@gmail.com>
2021-05-19 00:00:13 -0700

    Added calculator class
    Includes Addition and Subraction

---
+++ src/Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input the first Number: ");
+		int x = sc.nextInt();
+		
+		System.out.println("Input the second number: ");
+		int y = sc.nextInt();
+		
+		System.out.println("The addition of the two numbers is: " + addition(x,y));
+		System.out.println("The subraction of the two numbers is: " + subraction(x,y));
+		
+		
+		sc.close();
+		
+		
+	}
+	
+	public static int addition(int a, int b) { 
+		
+		int result = a + b;		
+		return result;
+	}
+
+	public static int subraction(int a, int b) { 
+		
+		int result = a - b;		
+		return result;
+	}
+
+}
